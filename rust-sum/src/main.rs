702d55462ac04657d0d3c98228b853e74d7e8f55
Alex Ocanoaia <alexandru.ocanoaia@stud.acs.upb.ro>
2025-03-16 11:44:29 +0200

    rust-sum: Added rust-sum program

--- rust-sum/src/main.rs
+++ rust-sum/src/main.rs
@@ -1,3 +1,9 @@
+fn calculate_sum(a: u32, b: u32) -> u32 {
+    a + b
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = 4;
+    let b= 6;
+    println!("The sum is {}", calculate_sum(a, b))
 }
